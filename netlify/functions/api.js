46feceefe1358ec03a759c0ba6bc0057fae32944
Sachin Kumar <sachinrkumar1000@gmail.com>
2026-05-31 12:07:36 +0530

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
-```python
+python
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import resend
@@ -39,4 +39,3 @@ def send_email():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4150)
-```
